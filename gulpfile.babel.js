05c54693e2a6cae8def10c521dd9e2888b4fc8fa
rudolfsonjunior <rudolfson.junior@gmail.com>
2017-03-13 19:23:24 +0100

    update gulpfile, remove uglify

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,10 +1,8 @@
 import gulp from 'gulp';
 import babel from 'gulp-babel';
-import uglify from 'gulp-uglify';
 
 gulp.task('default', () => {
   return gulp.src('./lib/index.js')
-        .pipe(babel())
-        .pipe(uglify())
-        .pipe(gulp.dest('./dist'));
+    .pipe(babel())
+    .pipe(gulp.dest('./dist'));
 });
