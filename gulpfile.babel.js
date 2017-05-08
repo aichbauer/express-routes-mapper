6c7993a643f7395eb601337cf3aaf19f8d1b3646
rudolfsonjunior <rudolfson.junior@gmail.com>
2017-05-08 14:14:26 +0200

    Chore: moved from gulp to npm scripts with babel-cli

--- gulpfile.babel.js
+++
@@ -1,8 +0,0 @@
-import gulp from 'gulp';
-import babel from 'gulp-babel';
-
-gulp.task('default', () => {
-  return gulp.src('./lib/index.js')
-    .pipe(babel())
-    .pipe(gulp.dest('./dist'));
-});
