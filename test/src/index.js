255952c1082f10b6150f2f77db4bcf9123ca75f0
Tochukwu Nkemdilim <nkemdilimtochukwu@gmail.com>
2018-11-23 15:32:08 +0100

    Chore(Test): Add Test Cases For Function Passed As General Middleware (Fixes #20)

--- test/src/index.js
+++ test/src/index.js
@@ -198,6 +198,85 @@ test('testing general middleware', (t) => {
   t.is('function', typeof (router));
 });
 
+test('testing general middleware recieved as function', (t) => {
+  const router = mapRoutes(routes, 'test/fixtures/controllers/', middlewareGeneral);
+
+  t.is(router.stack.length, 6);
+
+  // CLASS EXPORT DEFAULT
+  // method
+  t.is('get', router.stack[0].route.stack[0].method);
+  // route
+  t.is('/user', router.stack[0].route.path);
+  // function name
+  t.is('middlewareGeneral', router.stack[0].route.stack[0].name);
+  // function to call
+  t.is('function', typeof (router.stack[0].route.stack[0].handle));
+
+  // CLASS MODULE EXPORTS
+  // method
+  t.is('post', router.stack[1].route.stack[0].method);
+  // route
+  t.is('/user/:name', router.stack[1].route.path);
+  // keys for route
+  t.is('name', router.stack[1].keys[0].name);
+  // function name
+  t.is('middlewareGeneral', router.stack[1].route.stack[0].name);
+  // function to call
+  t.is('function', typeof (router.stack[1].route.stack[0].handle));
+
+  // FUNCTION MODULE EXPORTS
+  // method
+  t.is('put', router.stack[2].route.stack[0].method);
+  // route
+  t.is('/user/:name/:id', router.stack[2].route.path);
+  // keys for route
+  t.is('name', router.stack[2].keys[0].name);
+  t.is('id', router.stack[2].keys[1].name);
+  // function name
+  t.is('middlewareGeneral', router.stack[2].route.stack[0].name);
+  // function to call
+  t.is('function', typeof (router.stack[2].route.stack[0].handle));
+
+  // FUNCTION EXPORT DEFAULT
+  // method
+  t.is('delete', router.stack[3].route.stack[0].method);
+  // route
+  t.is('/user/:name/:id', router.stack[3].route.path);
+  // keys for route
+  t.is('name', router.stack[3].keys[0].name);
+  t.is('id', router.stack[3].keys[1].name);
+  // function name
+  t.is('middlewareGeneral', router.stack[3].route.stack[0].name);
+  // function to call
+  t.is('function', typeof (router.stack[3].route.stack[0].handle));
+
+  // MIDDLEWARES
+  // route
+  t.is('/user/middleware', router.stack[4].route.path);
+  t.is('/user/one-middleware', router.stack[5].route.path);
+  // parent method
+  t.is('get', router.stack[4].route.stack[0].method);
+  t.is('get', router.stack[4].route.stack[1].method);
+  t.is('get', router.stack[5].route.stack[0].method);
+  // keys for route
+  t.is(0, router.stack[4].route.stack[0].keys.length);
+  t.is(0, router.stack[4].route.stack[0].keys.length);
+  t.is(0, router.stack[4].route.stack[1].keys.length);
+  t.is(0, router.stack[4].route.stack[1].keys.length);
+  t.is(0, router.stack[5].route.stack[0].keys.length);
+  // function name
+  t.is('middlewareGeneral', router.stack[4].route.stack[0].name);
+  t.is('middleware1', router.stack[4].route.stack[1].name);
+  t.is('middlewareGeneral', router.stack[5].route.stack[0].name);
+  // function to call
+  t.is('function', typeof (router.stack[4].route.stack[0].handle));
+  t.is('function', typeof (router.stack[4].route.stack[1].handle));
+  t.is('function', typeof (router.stack[5].route.stack[0].handle));
+
+  t.is('function', typeof (router));
+});
+
 test('private and public are seperated', (t) => {
   const router = mapRoutes(privateRoutes, 'test/fixtures/controllers/');
   const router2 = mapRoutes(publicRoutes, 'test/fixtures/controllers/');
